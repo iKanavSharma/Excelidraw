81cd875f8ee74b5f85b2897a69d322a722cc4aad
Kanav Sharma <sharmakanavofficial05@gmail.com>
2025-09-26 22:12:03 +0530

    Completed backend

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,139 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware";
+import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types";
+import {prismaClient} from "@repo/db/client"
+
+const app=express();
+
+app.use(express.json());
+
+app.post("/signup", async (req,res)=>{
+    const parsedData=CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
+
+    try{
+        const user=await prismaClient.user.create({
+            data:{
+                email:parsedData.data.userName,
+                password:parsedData.data.password,
+                name:parsedData.data.name
+            }
+        })
+        //db call
+        res.json({
+            userId:user.id
+        })
+    }catch(e){
+        res.status(411).json({
+            message:"User already exist with this username"
+        })
+    }
+   
+    
+})
+
+app.post("/signin", async (req,res)=>{
+
+    const parsedData=SigninSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            message:"Incorrect Input"
+        })
+        return;
+    }
+
+    const user=await prismaClient.user.findFirst({
+
+        where:{
+            email:parsedData.data.userName,
+            password:parsedData.data.password
+        }
+
+    })
+
+    if(!user){
+        res.json({
+            message:"Not authorisied"
+        })
+        return;
+    }
+
+    const token=jwt.sign({
+        userId:user?.id
+    },JWT_SECRET);
+
+    res.json({
+        token
+    })
+
+})
+
+app.post("/room",middleware,async (req,res)=>{
+
+    const parsedData=CreateRoomSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
+    //@ts-ignore TODO:global type
+    const userId=req.userId;
+
+    try{
+        const room=await prismaClient.room.create({
+        data:{
+            slug:parsedData.data.name,
+            adminId:userId
+        }
+        })
+        //db call
+        res.json({
+            roomId:room.id
+        })
+    }catch(e){
+        res.status(411).json({
+            message:"Room already exists with this name"
+        })
+    }
+    
+})
+
+app.get("/chats/:roomId",async (req,res)=>{
+    const roomId=Number(req.params.roomId);
+    const messages=await prismaClient.chat.findMany({
+        where:{
+            roomId:roomId
+        },
+        orderBy:{
+            id:"desc"
+        },
+        take:50
+    });
+    res.json({
+        messages
+    })
+})
+
+app.get("/room/:slug",async (req,res)=>{
+    const slug=req.params.slug;
+    const room=await prismaClient.room.findFirst({
+        where:{
+            slug
+        }
+    });
+    res.json([
+        room
+    ])
+})
+
+console.log("Listening on 3001");
+
+app.listen(3001);
---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,110 @@
+import { WebSocket, WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import {JWT_SECRET} from "@repo/backend-common/config";
+import {prismaClient} from "@repo/db/client";
+
+const wss=new WebSocketServer({port:8080});
+
+interface User{
+    ws:WebSocket,
+    rooms:string[],
+    userId:string
+}
+
+const users:User[]=[];
+
+function checkUser(token:string):string | null{
+    try{
+        const decoded=jwt.verify(token,JWT_SECRET)
+
+        if(typeof decoded=="string"){
+            return null;
+        }
+
+        if(!decoded || !decoded.userId){
+            return null;
+        }
+        return decoded.userId;
+    }catch(e){
+        console.log(e);
+        return null;
+    }
+    return null;
+}
+
+wss.on('connection',function connection(ws,request){
+    console.log("hi");
+    const url=request.url;
+    if(!url){
+        return;
+    }
+    console.log("url found " + url);
+    const queryParams=new URLSearchParams(url.split('?')[1]);
+    console.log(queryParams);
+    console.log();
+    const token=queryParams.get('token') ?? "";
+    console.log(token);
+    console.log();
+    const userId=checkUser(token);
+    console.log(userId);
+    console.log("passed");
+    if(userId==null){
+        ws.close();
+        return null;
+    }
+    //constrol reached here,we have access to userId 
+    //store user in global arry
+    users.push({
+        userId,
+        rooms:[],
+        ws
+    })
+
+    ws.on('message',async function message(data){
+        //type check for data
+        
+        const parsedData=JSON.parse(data as unknown as string);//{type:"join-room ,roomId:1"}
+        //if message is join room find the user in global user array and to that rooms [] push room id
+        if(parsedData.type==="join_room"){
+            const user=users.find(x=>x.ws===ws);
+            // if(!user){
+            //     return;
+            // }
+            user?.rooms.push(parsedData.roomId);
+        }
+
+        if(parsedData.type==="leave_room"){
+            const user=users.find(x=>x.ws===ws);
+            if(!user){
+                return;
+            }
+            //removing specific room id from room array
+            user.rooms=user?.rooms.filter(x=>x===parsedData.room);
+        }
+
+        if(parsedData.type==="chat"){
+            const roomId=parsedData.roomId;
+            const message=parsedData.message;
+
+            await prismaClient.chat.create({
+                data:{
+                    roomId,
+                    message,
+                    userId
+                }
+            })
+
+            users.forEach(user=>{
+                if(user.rooms.includes(roomId)){
+                    user.ws.send(JSON.stringify({
+                        type:"chat",
+                        message:message,
+                        roomId
+                    }))
+                }
+            })
+            
+        }
+
+    });
+});
