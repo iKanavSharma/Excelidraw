ed9417675ade3bbd6a05d971cb3016383759454f
Kanav Sharma <sharmakanavofficial05@gmail.com>
2025-10-01 23:46:14 +0530

    Added Sigin page

--- apps/ecelidraw-frontend/app/signin/page.tsx
+++ apps/ecelidraw-frontend/app/signin/page.tsx
@@ -6,21 +6,38 @@ import { useState } from "react";
 
 
 export default function Signin(){
-    const [email,setEmail]=useState("");
+    const [userName,setuserName]=useState("");
     const [password,setPassword]=useState("");
     const navigation=useRouter();
     return <div className="flex justify-center items-center min-h-screen bg-background bg-gray-300">
         <div className="w-full max-w-md p-8 space-y-6 bg-card bg-white rounded-2xl shadow-lg">
             <h2 className="text-2xl font-bold text-center">Sign in to your account</h2>
             <form className="space-y-4">
-                <input type="email" placeholder="Email" value={email} className="w-full px-4 py-2 border rounded-md bg-input text-foreground focus:ring-2 focus:ring-primary" onChange={(e)=>{
-                    setEmail(e.target.value)
+                <input type="email" placeholder="Email" value={userName} className="w-full px-4 py-2 border rounded-md bg-input text-foreground focus:ring-2 focus:ring-primary" onChange={(e)=>{
+                    setuserName(e.target.value)
                 }}/>
                 <input type="password" placeholder="Password" value={password} className="w-full px-4 py-2 border rounded-md bg-input text-foreground focus:ring-2 focus:ring-primary" onChange={(e)=>{
                     setPassword(e.target.value)
                 }}/>
-                <button type="submit" className="w-full bg-blue-500 py-2 px-4 bg-primary text-primary foreground rounded-md hover:bg-primary/90" onClick={()=>{
-                    axios.post(`${HTTP_BACKEND}/signin`);
+                <button type="submit" className="w-full bg-blue-500 py-2 px-4 bg-primary text-primary foreground rounded-md hover:bg-primary/90" onClick={async (e)=>{
+                    e.preventDefault();
+                    const response=await axios.post("http://localhost:3001/signin",{
+                        userName,
+                        password
+                    });
+                    const token=response.data.token
+                    localStorage.setItem("token",token);//store token
+                    //create room
+                    const room=await axios.post("http://localhost:3001/room",{
+                        name:`${userName}-room`
+                    },{
+                        headers:{
+                            Authorization:`${token}`
+                        }
+                    })
+                    //roomid 
+                    const roomId=room.data.id;
+                    navigation.push(`/canvas/${roomId}`);
                 }}>
                 Sign In
                 </button>
@@ -33,6 +50,4 @@ export default function Signin(){
     </div>
 }
 
-function useNavigate() {
-    throw new Error("Function not implemented.");
-}
+
