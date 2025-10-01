94ce34d30261e429d8d70cea89335dbd70a5c793
Kanav Sharma <sharmakanavofficial05@gmail.com>
2025-10-01 23:08:42 +0530

    Modified Sign up page

--- apps/ecelidraw-frontend/app/signup/page.tsx
+++ apps/ecelidraw-frontend/app/signup/page.tsx
@@ -3,9 +3,10 @@
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+ 
 
 export default function Signup(){
-    const [email,setEmail]=useState("");
+    const [userName,setuserName]=useState("");
     const [password,setPassword]=useState("");
     const [fullName,setFullName]=useState("");
     const navigate=useRouter();
@@ -13,8 +14,8 @@ export default function Signup(){
         <div className="w-full max-w-md p-8 space-y-6 bg-card bg-white rounded-2xl shadow-lg">
             <h2 className="text-2xl font-bold text-center">Sign Up</h2>
             <form className="space-y-6">
-                <input type="email" placeholder="Email" value={email} className="px-2 py-2 w-full border rounded-md" onChange={(e)=>{
-                    setEmail(e.target.value);
+                <input type="email" placeholder="Email" value={userName} className="px-2 py-2 w-full border rounded-md" onChange={(e)=>{
+                    setuserName(e.target.value);
                 }}/>
 
                 <input type="password" placeholder="Password" value={password} className="px-2 py-2 w-full border rounded-md" onChange={(e)=>{
@@ -25,23 +26,14 @@ export default function Signup(){
                     setFullName(e.target.value);
                 }}/>
                 <div className="pt-4">
-                    <button className="bg-blue-500 w-full border px-1 py-1 rounded-md cursor-pointer" onClick={async ()=>{
+                    <button className="bg-blue-500 w-full border px-1 py-1 rounded-md cursor-pointer" onClick={async (e)=>{
+                        e.preventDefault();
                         const response=await axios.post("http://localhost:3001/signup",{
-                            email,
+                            userName,
                             password,
                             name:fullName
                         });
-                        localStorage.setItem("token",response.data.token);
-                        //creating room just after signup 
-                        const room=await axios.post("http://localhost:3001/room",{
-                            name:`${fullName}-room`
-                        },{
-                            headers:{
-                                Authorization: `Bearer ${response.data.token}`
-                            }
-                        })
-                        const roomId=room.data.roomId
-                        navigate.push(`/canvas/$[roomId]`);
+                        navigate.push("/signin");
                     }}>Sign Up</button>
                     <p className="text-center text-sm text-muted-foreground py-2">
                         Already  have an account?<a href="/signin" className="text-primary hover:underline hover:text-red-600">Sign In</a>
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -3,13 +3,17 @@ import { JWT_SECRET } from "@repo/backend-common/config";
 import jwt from "jsonwebtoken";
 
 export function middleware(req:Request,res:Response,next:NextFunction){
+    console.log("hi");
     const token=req.headers["authorization"] ?? "";
-
+    console.log(token);
     const decoded=jwt.verify(token,JWT_SECRET);
+    console.log(decoded)
     //add global types
     if(decoded){
         //@ts-ignore
         req.userId=decoded.userId;
+        //@ts-ignore
+        console.log(req.userId);
         next();
     }else{
         res.status(403).json({
